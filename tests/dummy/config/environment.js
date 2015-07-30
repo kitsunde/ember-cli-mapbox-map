f914c12026f78f6de1d88a0ec2f852511d2132d5
Igor Preston <igorpreston@gmail.com>
2015-07-30 20:56:16 +0300

    added addon configuration options

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -19,6 +19,12 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['mapbox'] = {
+    accessToken: '',
+    mapId: '',
+    mapTag: 'map'
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
