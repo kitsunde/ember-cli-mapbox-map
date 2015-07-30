c354854b521ee0e3ec76db3715681e787932a624
Igor Preston <igorpreston@gmail.com>
2015-07-30 20:58:12 +0300

    implemented mapbox instance initializer

--- addon/instance-initializers/mapbox.js
+++ addon/instance-initializers/mapbox.js
@@ -1,5 +1,9 @@
 export function initialize(instance) {
-
+  let config = instance.container.lookupFactory('config:environment');
+  let service = instance.container.lookup('mapbox:main');
+  service.set('accessToken', config.mapbox.accessToken);
+  service.set('mapId', config.mapbox.mapId);
+  service.set('mapTag', config.mapbox.mapTag);
 }
 
 export default {
