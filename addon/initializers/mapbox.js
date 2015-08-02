7e8a26b8a6214d5abf82246f3058da1d75ae1d52
Igor Preston <igorpreston@gmail.com>
2015-08-02 19:35:14 +0300

    implemented mapbox initializer

--- addon/initializers/mapbox.js
+++ addon/initializers/mapbox.js
@@ -4,6 +4,7 @@ export function initialize(registry, application) {
   registry.register("mapbox:main", Mapbox);
   application.inject('route', 'mapbox', 'mapbox:main');
   application.inject('controller', 'mapbox', 'mapbox:main');
+  application.inject('component:mapbox-map', 'mapbox', 'mapbox:main');
 }
 
 export default {
