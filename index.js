9c33a8345e1b41159435948785cde2cda5afaf54
Igor Preston <igorpreston@gmail.com>
2015-08-02 19:37:36 +0300

    implemented addon installation process

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-mapbox'
+  name: 'ember-cli-mapbox',
+
+  contentFor: function(type) {
+    if (type === 'head-footer') {
+      return "<link href='https://api.mapbox.com/mapbox.js/v2.2.1/mapbox.css' rel='stylesheet' />";
+    };
+
+    if (type === 'body-footer') {
+      return "<script src='https://api.mapbox.com/mapbox.js/v2.2.1/mapbox.js'></script>";
+    };
+  }
 };
