77c138e3de41fc3a255d06f53760b7263b3df4e4
Igor Preston <igorpreston@gmail.com>
2015-08-06 17:05:29 +0300

    added markers as model on dummy app route

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -7,5 +7,52 @@ export default Ember.Controller.extend({
            marker.feature.properties.title +
            '<br/><strong>Address:</strong> ' + marker.feature.properties.description +
            '<br/><span style="color:red;">This is a red text.</span>';
+  },
+
+  markers: [{
+    // this feature is in the GeoJSON format: see geojson.org
+    // for the full specification
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        // coordinates here are in longitude, latitude order because
+        // x, y is the standard for GeoJSON and many formats
+        coordinates: [
+          -77.03221142292,
+          38.913371603574
+        ]
+    },
+    properties: {
+        title: 'Peregrine Espresso',
+        description: '1718 14th St NW, Washington, DC',
+        // one can customize markers by adding simplestyle properties
+        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+        'marker-size': 'large',
+        'marker-color': '#BE9A6B',
+        'marker-symbol': 'cafe'
+    }
+},
+{
+  // this feature is in the GeoJSON format: see geojson.org
+  // for the full specification
+  type: 'Feature',
+  geometry: {
+      type: 'Point',
+      // coordinates here are in longitude, latitude order because
+      // x, y is the standard for GeoJSON and many formats
+      coordinates: [
+        -77.03221142292,
+        37.913371603574
+      ]
+  },
+  properties: {
+      title: 'pizza locca',
+      description: '1718 14th St NW, Washington, DC',
+      // one can customize markers by adding simplestyle properties
+      // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+      'marker-size': 'large',
+      'marker-color': '#BE9A6B',
+      'marker-symbol': 'cafe'
   }
+}]
 });
